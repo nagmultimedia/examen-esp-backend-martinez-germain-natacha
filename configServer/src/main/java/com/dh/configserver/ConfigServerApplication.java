7578286f367cf1fefb271747de231d7e08531a40
Natacha <nag.multimedia@gmail.com>
2023-05-25 20:27:37 -0300

    se suben los archivos que corren bien, el config server, el gateway, el aureka y el pom modificado

---
+++ configServer/src/main/java/com/dh/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.dh.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
